a86b620225a097695a774e10bb246c7779ddeb7d
Steph Samson <sdvsamson@gmail.com>
2014-01-15 04:14:17 +1100

    Remove trailing newline

--- mincss.rb
+++ mincss.rb
@@ -7,10 +7,13 @@ class CSS
 
 	def clean_file
 		@mincss = File.open("#{@file_name_min}.min.css", "w")
-		File.open(@file_name, "r") do |line|
-			line.each_line { |f| @mincss.puts(f.gsub(/\s+/, ""))}
+		File.open(@file_name, "r") do |file|
+			file.each_line do |line|
+				line.each_line { |f| @mincss.print(f.gsub(/\s+/, ""))}
+			end
 		end
-	end
+	end 
 end
+
 # Edit 'style.css' to your CSS file name.
 CSS::new('style.css')
