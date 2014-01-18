9c64d87385919dac91bf509c3ba538abbdd026bf
Steph Samson <sdvsamson@gmail.com>
2014-01-19 01:29:19 +1100

    Clean methods

--- lib/mincss.rb
+++ lib/mincss.rb
@@ -5,20 +5,26 @@ module Mincss
   	def initialize(file)
   		@file_name = file
   		@file_name_min = file.clone.gsub(/(.css)/, "")
-  		clean_file
+  		min_generate
   	end	
 
-  	def clean_file
+  	def min_generate
   		@mincss = File.open("#{@file_name_min}.min.css", "w")
   		File.open(@file_name, "r") do |file|
   			file.each_line do |line|
-  				line.each_line do |part|
-            @mincss.print part
-            @mincss.print " " if part[/\w/]
-          end
-  			end
+          initial_clean
+        end
   		end
   	end 
+  
+    def initial_clean
+      line.each_line do |part|
+          @mincss.print part
+          @mincss.print " " if part[/\w/]
+        end
+      end
+    end
+
   end
 
   begin
