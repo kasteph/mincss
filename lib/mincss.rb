e7b26ddd42b6db3ace47bf57b2e6ce69b1cbd863
Mark Wetzel <mark@markwetzel.com>
2014-01-19 13:48:16 -0500

    Refactoring.

--- lib/mincss.rb
+++ lib/mincss.rb
@@ -9,12 +9,10 @@ module Mincss
     SELECTOR_COMMA    = ","
     PROPERTY_OPENING  = ":"
     PROPERTY_CLOSING  = ";"
-    WHITESPACE        = " "
-    EMPTY             = ""
     
   	def initialize(filename)
   		@filename = filename
-  		@filename_min = filename.clone.gsub(/(.css)/, '')
+  		@filename_min = filename.clone.gsub(/(.css)/, "")
       @inside_property = false
       @inside_selector = false
   		clean_file
@@ -22,95 +20,86 @@ module Mincss
 
   	def clean_file
   		content = File.read(@filename)          
-      formatted = remove_tabs(content)
-      formatted = remove_newlines(formatted)      
-      # reduce all spaces to one space for initial processing.
-      content.gsub!(/\s+/, ' ')      
-      formatted = remove_whitespace(formatted)
+      formatted = strip_tabs(content)
+      formatted = strip_newlines(formatted)      
+      formatted = process_whitespace(formatted)
       
       write_file(formatted)
   	end 
     
     def write_file(content)  
-      File.open("#{@filename_min}.min.css", 'w') do |file|
+      File.open("#{@filename_min}.min.css", "w") do |file|
         file.write(content)
       end
     end
     
-    def remove_tabs(content)
-      content.gsub!("\t", "")
+    def strip_tabs(content)
+      content.gsub!("\t",  "")
     end
         
-    def remove_newlines(content)
+    def strip_newlines(content)
       content.gsub!("\n", "")
     end
     
     def preserve_whitespace(content, char)
       if property_opening?(char)
-        
-      elsif selector_opening?(char)
-        
+        @inside_property = true
+      elsif property_closing?(char)          
+        if content[-1] == " "
+          content[-1] = ""
+        end          
+        @inside_property = false
       end
       
-      if property_closing?(char) || selector_closing
-
-
+      if selector_opening?(char)
+        @inside_selector = true
+      elsif selector_closing?(char)
+        if content[-1] == " "
+          content[-1] = ""
+        end
+        @inside_selector = false
       end
+      
+      content
     end
     
-    def remove_whitespace(content)
+    def strip_whitespace(formatted, char, last_char)      
+      if whitespace?(char)          
+        if @inside_property            
+          if last_char == PROPERTY_OPENING
+            formatted << ""
+          else
+            formatted << char
+          end
+        elsif @inside_selector
+          if last_char == SELECTOR_CLOSING || last_char == SELECTOR_COMMA
+            formatted << ""
+          else
+            formatted << char
+          end
+        end
+      else        
+        formatted << char
+      end
+      
+      formatted
+    end
+    
+    def process_whitespace(content)
       formatted = ""
       char = ""
-      last_char = ""
+      last_char = ""      
       
-      content.each_char do |char|        
-        if property_opening?(char)
-          @inside_property = true
-        elsif property_closing?(char)          
-          if formatted[-1] == WHITESPACE
-            formatted[-1] = EMPTY
-          end          
-          @inside_property = false
-        end
-        
-        if selector_opening?(char)
-          @inside_selector = true
-        elsif selector_closing?(char)
-          if formatted[-1] == WHITESPACE
-            formatted[-1] = EMPTY
-          end
-          @inside_selector = false
-        end
-        
-        # content = preserve_whitespace(content, char)
-
-        
-        if whitespace?(char)          
-          if @inside_property            
-            if last_char == ':'
-              formatted << ""
-            else
-              formatted << char
-            end
-          elsif @inside_selector
-            if last_char == SELECTOR_CLOSING || last_char == SELECTOR_COMMA
-              formatted << ""
-            else
-              formatted << char
-            end
-          end
-        else        
-          formatted << char
-        end
-        
+      # reduce all spaces to one space for initial processing.
+      content.gsub!(/\s+/, " ")      
+            
+      content.each_char do |char|       
+        formatted = preserve_whitespace(formatted, char)
+        formatted = strip_whitespace(formatted, char, last_char)              
         last_char = char
       end
            
       formatted
-    end
-    
-    def last_char(content)
-      content[-1]
     end
     
     def selector_opening?(char)
@@ -122,11 +111,11 @@ module Mincss
     end
     
     def property_opening?(char)
-      char == PROPERTY_OPENING
+      char == PROPERTY_OPENING && !@inside_selector
     end
     
     def property_closing?(char)
-      char == PROPERTY_CLOSING
+      char == PROPERTY_CLOSING && !@inside_selector
     end
     
     def newline?(char)
